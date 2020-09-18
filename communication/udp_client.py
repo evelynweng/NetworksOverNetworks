cc85694f6e5cb2aba73c9bf2498a1ca8dfeafbf5
Helen <helen.zheng@sjsu.edu>
2020-09-18 14:50:59 -0700

    Changed udp_client to take ip address and port as inputs.

--- communication/udp_client.py
+++ communication/udp_client.py
@@ -18,8 +18,11 @@ UDP_CLIENT = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # server_info[0] should be the server's ip address, server_info[1] should be the port that is hosting on the server
 # 35.161.72.250 us my ec2 public ipv4 address
 # 9999 is my ec2 host port
-server_info = ("127.0.0.1", 9999)
-input_str = input("Plese input text you would like to serizalizing: ")
+ip_address = input("Please input the IP address to send: ")
+port = input("Please input the port of the IP address to send: ")
+server_info = (ip_address, int(port))
+#server_info = ("34.123.189.50", 9999)
+input_str = input("Please input text you would like to serialize: ")
 BUFFER_SIZE = 1024
 msg_encrpted = serializer.serialize(input_str)
 UDP_CLIENT.sendto(msg_encrpted.encode(), server_info)
