9fa7d25c50cbca2e2f1e6f4731e0ab11eed3fbd0
Helen Z <54555607+hzheng55@users.noreply.github.com>
2020-09-18 20:53:30 -0700

    Revert "Changed udp_client to take ip address and port as inputs."

--- communication/udp_client.py
+++ communication/udp_client.py
@@ -8,26 +8,20 @@ from data_representation.serialize import Serialization
 serializer = Serialization()
 deserializer = Deserialization()
 
-class Client():
 
-    """
-    this is a example for socket programing on udp server. 
-    """
-    def udp_client(self, ip_address, port, message):
-        # create a socket object
-        UDP_CLIENT = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+"""
+this is a example for socket programing on udp server
+"""
+# create a socket object
+UDP_CLIENT = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        # server_info[0] should be the server's ip address, server_info[1] should be the port that is hosting on the server
-        # 35.161.72.250 us my ec2 public ipv4 address
-        # 9999 is my ec2 host port
-        
-        #ip_address = input("Please input the IP address to send: ")
-        #port = input("Please input the port of the IP address to send: ")
-        server_info = (ip_address, int(port))
-        #server_info = ("34.123.189.50", 9999)
-        #input_str = input("Please input text you would like to serialize: ")
-        BUFFER_SIZE = 1024
-        msg_encrpted = serializer.serialize(message)
-        UDP_CLIENT.sendto(msg_encrpted.encode(), server_info)
-        msg = UDP_CLIENT.recvfrom(BUFFER_SIZE)
-        print(msg[0].decode())
+# server_info[0] should be the server's ip address, server_info[1] should be the port that is hosting on the server
+# 35.161.72.250 us my ec2 public ipv4 address
+# 9999 is my ec2 host port
+server_info = ("127.0.0.1", 9999)
+input_str = input("Plese input text you would like to serizalizing: ")
+BUFFER_SIZE = 1024
+msg_encrpted = serializer.serialize(input_str)
+UDP_CLIENT.sendto(msg_encrpted.encode(), server_info)
+msg = UDP_CLIENT.recvfrom(BUFFER_SIZE)
+print(msg[0].decode())
