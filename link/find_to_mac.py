285e948aee3db3563ede86869af92790cb27b651
ruiyang <reyyang00@gmail.com>
2020-09-26 21:28:58 -0700

    combinted Asad and  Evelyn versions together, sucessfully forward packets in 8 nodes on top of aws ec2 instances

---
+++ link/find_to_mac.py
@@ -0,0 +1,21 @@
+import yaml
+​
+TOPOLOGY = 'sequential_topology'
+​
+class Find_to_mac():
+    def __init__(self):
+        with open("link/connection_config.yaml") as fl:
+            self.config = yaml.load(fl)[TOPOLOGY]
+​
+    def find_mac(self, to_label):
+      
+        # import connection
+        # only connection now eve<->evelyn<->kitty
+        to_label = to_label.lower()
+        if to_label not in self.config['global_routing_table']:
+            return None
+        direction = self.config['global_routing_table'][to_label]
+        if direction not in self.config['left_right_neighbors']:
+            return None
+​
+        return self.config['left_right_neighbors'][direction]
---
+++ physical/tcp_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import socket
+class Tcp_server():
+    def __init__(self):
+        self.TCP_IP = socket.gethostname() #default local host name
+        self.TCP_PORT = 9999
+        self.BUFFER_SIZE = 1024  # Normally 1024
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((self.TCP_IP, self.TCP_PORT))
+        self.s.listen(1)
+        print ("start listen")
+
+    def recv(self):
+        print("server waits for connection ..")
+        self.conn, self.addr = self.s.accept()
+        print ('Connection address:', self.addr)
+        data = self.conn.recv(self.BUFFER_SIZE)
+        if data:
+            data = data.decode()
+            print ("received data:", data)
+            self.conn.send("router ack".encode())  # echo
+        self.conn.close()
+        return data
