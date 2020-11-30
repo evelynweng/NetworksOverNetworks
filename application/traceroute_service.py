e3898428b9f358313fe75e687c6b592b114be95e
Arturo Montoya <montoya332@live.com>
2020-11-30 11:07:32 -0800

    on message

--- application/traceroute_service.py
+++ application/traceroute_service.py
@@ -3,11 +3,8 @@ from data_parse.network_message import NetworkMessage
 
 class Traceroute_service():
     def traceroute_send_service(self, mylabel, to_label):
-        to_network_layer = []
-        for i in range(1, 12):
-            msg = NetworkMessage() \
-                .set_from_label(mylabel).set_to_label(to_label) \
-                .set_identifier('traceroute').set_acknowledgment_number('SYNC')\
-                .set_sequence_number(i).set_start_time_now().set_ttl(i).set_to_label_original(to_label)
-            to_network_layer.append(msg.get_data())
-        return to_network_layer
+        msg = NetworkMessage() \
+            .set_from_label(mylabel).set_to_label(to_label) \
+            .set_identifier('traceroute').set_acknowledgment_number('SYNC')\
+            .set_sequence_number(1).set_start_time_now().set_ttl(1).set_to_label_original(to_label)
+        return msg.get_data()
